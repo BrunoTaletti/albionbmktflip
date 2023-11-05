222b4b84488e695be4294f5292997e49e0dacdd3
System Administrator <root@MacBook-Air-de-Bruno.local>
2023-11-05 20:58:50 -0300

    correcoes

--- scripts/albion-market-api.js
+++ scripts/albion-market-api.js
@@ -11,25 +11,35 @@ document.getElementById("priceComparisonForm").addEventListener("submit", functi
     }
     
     const endpoint = `https://west.albion-online-data.com/api/v2/stats/prices/${encodeURIComponent(itemFullName)}?locations=Caerleon,Blackmarket&qualities=${quality}`;
-    
+
+    const itemIcon = document.getElementById("item-photo"); // Mova a criação da imagem para fora do loop
+
     fetch(endpoint)
         .then(response => response.json())
         .then(data => {
             const priceTable = document.getElementById("priceTable");
             const tbody = priceTable.querySelector("tbody");
             tbody.innerHTML = "";
-            
+
             for (const location in data) {
                 const priceData = data[location];
                 const newRow = document.createElement("tr");
                 newRow.innerHTML = `
-                    <td>${priceData.item_id}</td>
+                    <img class="item-photo"/>
                     <td>${priceData.quality}</td>
                     <td>${priceData.city}</td>
                     <td>$${priceData.sell_price_min}</td>
                     <td>$${priceData.sell_price_max}</td>
                 `;
+
+                const itemPhoto = newRow.querySelector(".item-photo"); // Encontre a imagem dentro da nova linha
+                itemPhoto.src = `https://render.albiononline.com/v1/item/${itemFullName}.png`;
+
                 tbody.appendChild(newRow);
+            }
+
+            if (itemIcon) {
+                itemIcon.src = `https://render.albiononline.com/v1/item/${itemFullName}.png`;
             }
         })
         .catch(error => console.error("Erro na busca de preços:", error));
--- scripts/albion-runas.js
+++ scripts/albion-runas.js
@@ -1,7 +1,7 @@
 function buscarPrecosDasRunas() {
     const runes = "T4_RUNE,T5_RUNE,T6_RUNE,T7_RUNE,T8_RUNE";
     const souls = "T4_SOUL,T5_SOUL,T6_SOUL,T7_SOUL,T8_SOUL";
-    const itemsName = runes + souls;
+    const itemsName = runes + ',' + souls;
     const currentDate = new Date().toJSON().slice(0, 10);
 
     const runeEndpoint = `https://west.albion-online-data.com/api/v2/stats/history/${itemsName}?end_date=${currentDate}&locations=Caerleon&time-scale=6`;
@@ -14,22 +14,24 @@ function buscarPrecosDasRunas() {
             tbody.innerHTML = "";
             
             for (const location in datas) {
-                const priceDataa = datas[location];
+                const runePriceData = datas[location];
                 const newRow = document.createElement("tr");
                 
                 newRow.innerHTML = `
-                    <td>${priceDataa.item_id}</td>
-                    <td>${priceDataa.location}</td>
+                    <img class="rune-photo"/>
+                    <td>${runePriceData.location}</td>
+                    <td>$${runePriceData.data[0].avg_price}</td>
+                    <td>${runePriceData.data[0].item_count}</td>
                 `;
                 
-                for (const dataItem of priceDataa.data) {
-                    newRow.innerHTML += `
-                        <td>$${dataItem.avg_price}</td>
-                        <td>${dataItem.item_count}</td>
-                    `;
-                }
-                
+                const runePhoto = newRow.querySelector(".rune-photo"); // Encontre a imagem dentro da nova linha
+                runePhoto.src = `https://render.albiononline.com/v1/item/${runePriceData.item_id}.png`;
+
                 tbody.appendChild(newRow);
+
+                if (runePhoto) {
+                    runePhoto.src = `https://render.albiononline.com/v1/item/${runePriceData.item_id}.png`;
+                }
             }
         })
         .catch(error => console.error("Erro na busca de preços das runas:", error));
